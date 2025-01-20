638dcf4ae71069cfc9d10bad958c7a79b0cb94f1
asharovatova <co.rin@mail.ru>
2025-01-20 14:52:33 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { readdir, stat } = require('fs/promises');
+
+const pathToSecretFolder = path.join(__dirname, 'secret-folder');
+
+async function showFilesInfo() {
+  try {
+    const entries = await readdir(pathToSecretFolder, { withFileTypes: true });
+
+    for (const entry of entries) {
+      if (entry.isFile()) {
+        const pathToFile = path.join(__dirname, 'secret-folder', entry.name);
+
+        const fileName = entry.name.split('.')[0];
+
+        const fileExtension = path.extname(pathToFile).slice(1);
+
+        let file = await stat(pathToFile);
+        let fileSize = file.size;
+
+        console.log(`${fileName} - ${fileExtension} - ${fileSize}kb`);
+      }
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+showFilesInfo();
