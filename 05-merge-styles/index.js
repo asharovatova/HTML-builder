90e6bb4bf823fc3ac709a32a667e98b6a3224632
asharovatova <co.rin@mail.ru>
2025-01-21 11:49:48 +0300

    feat: implement 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fsPromises = require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+async function mergeStyles() {
+  const stylesFolderPath = path.join(__dirname, 'styles');
+  const stylesFolder = await fsPromises.readdir(stylesFolderPath);
+  const destFolderPath = path.join(__dirname, 'project-dist');
+  const destFolder = await fsPromises.readdir(destFolderPath);
+  const destFilePath = path.join(__dirname, 'project-dist', 'bundle.css');
+
+  if (destFolder.length > 1) {
+    await fsPromises.rm(destFilePath);
+  }
+
+  const writeStream = fs.createWriteStream(destFilePath);
+
+  for (let fileName of stylesFolder) {
+    const pathToFile = path.join(stylesFolderPath, fileName);
+
+    if (path.extname(pathToFile) === '.css') {
+      const readStream = fs.createReadStream(pathToFile, 'utf-8');
+      readStream.on('data', (chunk) => writeStream.write(chunk.toString()));
+    }
+  }
+}
+
+mergeStyles();
